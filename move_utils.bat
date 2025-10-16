5378b66c5bca9c06d35bec44895b38cc4d931af2
Ryan Barbrick <barbrickdesign@gmail.com>
2025-10-16 09:26:30 -0400

    132

---
+++ move_utils.bat
@@ -0,0 +1,7 @@
+@echo off
+REM Move utility files to src/utils/
+move actual-website-fixer.js src\utils\
+move coinbase-wallet-integration.js src\utils\
+move deploy-automation.js src\utils\
+move local-scanner.js src\utils\
+echo Utility files moved successfully!
